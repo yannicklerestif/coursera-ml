7fb484538a5aa5ce366349d3183b43df66d606f7
yannicklerestif <yannick1976.git@gmail.com>
2016-07-17 22:28:46 +0200

    week 4 - init

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -83,7 +83,7 @@ fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
 alpha = 0.3;
-num_iters = 50;
+num_iters = 100;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -104,7 +104,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+normalized = [1; (1650 - mu(1))/sigma(1); (3 - mu(2))/sigma(2)];
+price = theta' * normalized; % You should change this
 
 
 % ============================================================
@@ -114,55 +115,6 @@ fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
-
-%% ============== Multiple values for alpha =================
-fprintf('Running mutliple gradient descents ...\n');
-
-% Choose some alpha value
-alphas = [0.3, 1];
-num_iters = 50;
-cmap = jet(length(alphas));
-
-disp('cmap -----------------------------');
-disp(cmap);
-disp('---------------------------');
-
-figure;
-hold on;
-for i = 1:length(alphas)
-    % Init Theta and Run Gradient Descent 
-    theta = zeros(3, 1);
-    [theta, J_history] = gradientDescentMulti(X, y, theta, alphas(i), num_iters);
-
-    % Plot the convergence graph
-    plot(1:numel(J_history), J_history, '-', 'LineWidth', 2, 'Color', cmap(i, :));
-    xlabel('Number of iterations');
-    ylabel('Cost J');
-
-    % Display gradient descent's result
-    fprintf('Theta computed from gradient descent: \n');
-    fprintf(' %f \n', theta);
-    fprintf('\n');
-
-    % Estimate the price of a 1650 sq-ft, 3 br house
-    % ====================== YOUR CODE HERE ======================
-    % Recall that the first column of X is all-ones. Thus, it does
-    % not need to be normalized.
-    price = 0; % You should change this
-end
-test = cell(length(alphas),1);
-for i=1:length(alphas)
-    test{i} = num2str(alphas(i));
-end
-legend(test);
-% ============================================================
-
-fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
-         '(using gradient descent):\n $%f\n'], price);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
 
 %% ================ Part 3: Normal Equations ================
 
@@ -198,7 +150,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+sample = [1; 1650; 3];
+price = theta' * sample; % You should change this
 
 
 % ============================================================
